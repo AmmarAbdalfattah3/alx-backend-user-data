5421a2d836156665af628ccf3e92955f255bb12c
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-04 01:46:59 +0000

    index

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
+
+
 from flask import jsonify, abort
 from api.v1.views import app_views
 
@@ -24,3 +26,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """ GET /api/v1/unauthorized
+    Trigger a 401 Unauthorized error
+    """
+    abort(401)
