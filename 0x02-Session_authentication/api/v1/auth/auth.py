851146a574d9c6cf4b647d2170ae0ca1d2a6b996
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-05 16:17:15 +0000

    app

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Auth class
+"""
+
+
+from typing import List, TypeVar
+from flask import Request
+
+
+User = TypeVar('User')
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required for a given path.
+        """
+        if path is None:
+            return True
+
+        if not excluded_paths or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request: Request = None) -> str:
+        """Return the authorization header"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request: Request = None) -> User:
+        """Return the current user"""
+        return None
