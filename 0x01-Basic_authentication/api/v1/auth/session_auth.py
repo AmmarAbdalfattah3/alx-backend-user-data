c9a7f7060abee941c80adb401c22313311ca0ebd
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-06 00:53:08 +0000

    session auth

--- 0x01-Basic_authentication/api/v1/auth/session_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/session_auth.py
@@ -32,3 +32,20 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+     def user_id_for_session_d(self, session_id: str = None) -> str:
+        """Retrieve a user ID based on a given session ID
+
+        Args:
+            session_id (str): The session ID to look up
+
+        Returns:
+            str: The user ID associated with the session ID,
+            None otherwise
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
