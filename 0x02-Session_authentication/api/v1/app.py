bde8fcf7f74a2b2915ee519fde598c2987e28400
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-08 13:08:39 +0000

    app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,7 +55,8 @@ def before_request():
     excluded_paths = [
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/'
+                '/api/v1/forbidden/',
+                '/api/v1/users/me'
             ]
     if not auth.require_auth(request.path, excluded_paths):
         return
