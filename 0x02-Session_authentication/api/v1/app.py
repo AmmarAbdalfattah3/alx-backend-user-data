d333d90bb5e5dcb6329761db5bc3b84b1db76fc8
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-09 12:48:42 +0000

    app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -20,9 +21,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
+
 if AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == 'session_auth':
+    auth = SessionAuth()
 else:
     auth = Auth()
 
