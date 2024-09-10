71d85db3e09721a3528294cde7036ef67201052d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-10 15:20:21 +0000

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,9 +42,7 @@ class DB:
         Returns:
             User: The created User object.
         """
-
         new_user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(new_user)
-        session.commit()
+        self._session.add(new_user)
+        self._session.commit()
         return new_user
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -13,3 +13,6 @@ print(user_1.id)
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
 print(user_2.id)
+
+if __name__ == "__main__":
+    main()
