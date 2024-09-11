cc841d77a8ae9a29a92177ba2d41312681b38690
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-11 13:59:03 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,20 +4,57 @@ Auth module
 """
 
 
+from db import DB
+from user import User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
-def _hash_password(password: str) -> bytes:
-    """Hash a password using bcrypt
-
-    Args:
-        password (str): The password to hash
-
-    Returns:
-        bytes: The salted hash of the password
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    password_bytes = password.encode('utf-8')
 
-    hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    def __init__(self):
+        self._db = DB()
 
-    return hashed_password
+    def _hash_password(self, password: str) -> bytes:
+        """Hash a password using bcrypt
+
+        Args:
+            password (str): The password to hash
+
+        Returns:
+            bytes: The salted hash of the password
+        """
+        password_bytes = password.encode('utf-8')
+
+        hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            User: The created User object.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            user = self._db.add_user(
+                        email=email,
+                        hashed_password=hashed_password.decode('utf-8')
+                    )
+            return user
+        except InvalidRequestError:
+            raise ValueError("Invalid request")
