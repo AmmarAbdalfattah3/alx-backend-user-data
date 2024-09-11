29ff2e1f4620e51a0b34d0f0eed292309930d684
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-11 14:10:26 +0000

    auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,12 +9,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
     """Handle GET requests to the root URL ("/")
        and return a JSON response
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
