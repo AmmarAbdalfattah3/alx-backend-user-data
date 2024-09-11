988b7fdf4a881a681386c2a41a2faf099ae1e848
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-11 17:16:56 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,26 @@ class Auth:
             hashed_password = self._hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates the login credentials.
+
+        Args:
+            mail (str): The email of the user.
+            password (str): The password provided by the user.
+
+        Returns:
+            bool: True if the login is valid, False otherwise.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+            if bcrypt.checkpw(
+                        password.encode('utf-8'),
+                        user.hashed_password.encode('utf-8')
+                    ):
+                return True
+        except NoResultFound:
+            return False
+        return False
