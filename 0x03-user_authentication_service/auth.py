bdab69131c4d5acc361ebb36d163044a075d7749
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-11 22:01:52 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,18 +21,19 @@ def _generate_uuid() -> str:
     """
     return str(uuid.uuid4())
 
+
 def _hash_password(password: str) -> bytes:
-        """Hash a password using bcrypt.
+    """Hash a password using bcrypt.
 
-        Args:
-            password (str): The password to hash.
+    Args:
+        password (str): The password to hash.
 
-        Returns:
-            bytes: The salted hash of the password.
-        """
-        password_bytes = password.encode('utf-8')
-        hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
-        return hashed_password
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    password_bytes = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed_password
 
 
 class Auth:
