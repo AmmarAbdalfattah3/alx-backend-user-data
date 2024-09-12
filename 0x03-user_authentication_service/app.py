598de500f031c03a035a8c589bac95c7d6e6cc00
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-12 12:41:11 +0000

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -148,7 +148,7 @@ def get_reset_password_token():
 
     try:
         reset_token = AUTH.get_reset_password_token(email)
-    except ValueError
+    except ValueError:
         abort(403, description="Email not registered")
 
     return jsonify({"email": email, "reset_token": reset_token}), 200
